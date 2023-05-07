b6e77c2cb55bd60ce20eb83fdb99b713ab71bf58
navindu-sachintha <navindu.social@gmail.com>
2023-05-08 00:45:33 +0530

    error: Module not defined

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect (()=> {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -26,10 +26,11 @@ const Nav = () => {
         <Image src="/assets/images/logo.svg" alt="promptify-logo" width={30} height={30} className="object-contain" />
         <p className="logo_text">Promptify</p>
       </Link>
+      
 
       {/* Desktop Nav */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -57,7 +58,7 @@ const Nav = () => {
 
       {/* Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image src="/assets/images/logo.svg" 
             width={37} 
